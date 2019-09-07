e14e86356c1e8ff099d9ef4adf2c82ec1936d21a
hunzsig <mzyhaohaoren@qq.com>
2019-09-08 00:01:13 +0800

    new api~

--- common/Api/Http.js
+++ common/Api/Http.js
@@ -1,4 +1,4 @@
-import { message } from 'antd';
+import {message} from 'antd';
 import axios from 'axios';
 import Crypto from '../Crypto';
 import Parse from '../Parse';
@@ -55,7 +55,8 @@ const Http = {
     const host = conf.host || null;
     const scope = conf.scope || null;
     const params = conf.params || {};
-    const then = conf.then || function () {};
+    const then = conf.then || function () {
+    };
     const crypto = conf.crypto || null;
     const header = conf.header || {};
     refresh = typeof refresh === 'boolean' ? refresh : false;
@@ -66,7 +67,7 @@ const Http = {
       return;
     }
     axios
-      .post(host, Crypto.encode({ client_id: Auth.getClientId(), scope: scope, ...params }, crypto), header)
+      .post(host, Crypto.encode({client_id: Auth.getClientId(), scope: scope, ...params}, crypto), header)
       .then((response) => {
         if (Crypto.is(crypto)) {
           response.data = Crypto.decode(response.data, crypto);
@@ -74,15 +75,11 @@ const Http = {
         if (typeof response.data === 'object') {
           if (typeof response.data.code === 'number' && response.data.code === 403) {
             if (Auth.getUid() !== undefined) {
-              if (response.data.msg === 'illegal permission online') {
-                message.error(Http.TipsLogin, 2.00, () => {
-                  Parse.locationTo(Http.PathLogin);
-                });
-              } else {
-                message.warning(response.data.msg + ' ^_^');
-              }
+              message.error(Http.TipsLogin, 2.00, () => {
+                Parse.locationTo(Http.PathLogin);
+              });
             }
-            then({ code: 500, response: I18n.translate('limitedOperation'), data: null });
+            then({code: 500, response: I18n.translate('limitedOperation'), data: null});
             return;
           }
           then(response.data);
@@ -90,33 +87,57 @@ const Http = {
             ApiSave(key, response.data);
           }
         } else {
-          then({ code: 500, response: I18n.translate('apiError'), data: null });
+          then({code: 500, response: I18n.translate('apiError'), data: null});
         }
       })
       .catch((error) => {
         const status = (error.response && error.response.status) ? error.response.status : -1;
         switch (status) {
-          case 400: error.message = I18n.translate('apiErrorQuery'); break;
-          case 401: error.message = I18n.translate('apiErrorNotAuth'); break;
-          case 403: error.message = I18n.translate('apiErrorReject'); break;
-          case 404: error.message = I18n.translate('apiErrorAbort'); break;
-          case 408: error.message = I18n.translate('apiErrorTimeout'); break;
-          case 500: error.message = I18n.translate('apiErrorServer'); break;
-          case 501: error.message = I18n.translate('apiErrorNotService'); break;
-          case 502: error.message = I18n.translate('apiErrorNet'); break;
-          case 503: error.message = I18n.translate('apiErrorServiceDisable'); break;
-          case 504: error.message = I18n.translate('apiErrorNetTimeout'); break;
-          case 505: error.message = I18n.translate('apiErrorNotSupportHttp'); break;
-          default: error.message = `${I18n.translate('apiErrorDefault')}(${status})!`;
+          case 400:
+            error.message = I18n.translate('apiErrorQuery');
+            break;
+          case 401:
+            error.message = I18n.translate('apiErrorNotAuth');
+            break;
+          case 403:
+            error.message = I18n.translate('apiErrorReject');
+            break;
+          case 404:
+            error.message = I18n.translate('apiErrorAbort');
+            break;
+          case 408:
+            error.message = I18n.translate('apiErrorTimeout');
+            break;
+          case 500:
+            error.message = I18n.translate('apiErrorServer');
+            break;
+          case 501:
+            error.message = I18n.translate('apiErrorNotService');
+            break;
+          case 502:
+            error.message = I18n.translate('apiErrorNet');
+            break;
+          case 503:
+            error.message = I18n.translate('apiErrorServiceDisable');
+            break;
+          case 504:
+            error.message = I18n.translate('apiErrorNetTimeout');
+            break;
+          case 505:
+            error.message = I18n.translate('apiErrorNotSupportHttp');
+            break;
+          default:
+            error.message = `${I18n.translate('apiErrorDefault')}(${status})!`;
         }
-        then({ code: status, response: error.message, data: null });
+        then({code: status, response: error.message, data: null});
       });
   },
   runAll: (conf, refresh) => {
     const host = conf.host || null;
     const scope = conf.scope || null;
     const params = conf.params || {};
-    const then = conf.then || function () {};
+    const then = conf.then || function () {
+    };
     const crypto = conf.crypto || null;
     const header = conf.header || {};
     refresh = typeof refresh === 'boolean' ? refresh : true;
@@ -138,7 +159,10 @@ const Http = {
         resultQty += 1;
       } else {
         realKey[idx] = key;
-        real.push(axios.post(host, Crypto.encode({ client_id: Auth.getClientId(), scope: s, ...(Array.isArray(params) ? params[idx] : params) }, crypto), header));
+        real.push(axios.post(host, Crypto.encode({
+          client_id: Auth.getClientId(),
+          scope: s, ...(Array.isArray(params) ? params[idx] : params)
+        }, crypto), header));
       }
     });
     if (resultQty === scope.length) {
@@ -167,7 +191,7 @@ const Http = {
               }
             }
           } else {
-            result[pushIdx] = { code: 500, response: I18n.translate('apiError'), data: null };
+            result[pushIdx] = {code: 500, response: I18n.translate('apiError'), data: null};
           }
         });
         if (hasNotAuth === true) {
